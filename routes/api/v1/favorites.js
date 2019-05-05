1e14bb84892aea570fc1c9d2e98d6a352503cab0
April Dagonese <adagonese@gmail.com>
2019-05-05 08:27:58 -0600

    Refactors favoritesRouter per Peter's feedback

--- routes/api/v1/favorites.js
+++ routes/api/v1/favorites.js
@@ -9,23 +9,31 @@ var fetch = require('node-fetch');
 
 /* POST favorite listing. */
 favoritesRouter.post("/", function(req, res, next) {
+  res.setHeader("Content-Type", "application/json");
   User.findOne({ where: { api_key: req.body.api_key } })
   .then(user => {
     if (user === null) {
       res.setHeader("Content-Type", "application/json");
       res.status(401).send(JSON.stringify("Invalid API Key"))
     } else {
-      var location = req.body.location
-      Location.findOrCreate({ where: {name: location} })
-      .then(([location, created]) => {
-        UserLocation.findOrCreate({ where: {UserId: user.id, LocationId: location.id} })
-        .then(([userLocation, created]) => {
-          res.setHeader("Content-Type", "application/json");
-          res.status(201).send(JSON.stringify({"message":`${location.name} has been added to your favorites`}));
-        })
-      })
+      getFavorite(user, req.body.location, res)
     };
   });
 });
+
+async function getFavorite(user, location, res) {
+  try {
+    Location.findOrCreate({ where: {name: location} })
+    .then(([location, created]) => {
+      return UserLocation.findOrCreate({ where: {UserId: user.id, LocationId: location.id} })
+    })
+    .then(([userLocation, created]) => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(201).send(JSON.stringify({"message":`${location} has been added to your favorites`}))
+    })
+  } catch (error) {
+    res.status(401).send(JSON.stringify("Could not store favorite at this time"))
+  }
+}
 
 module.exports = favoritesRouter;
